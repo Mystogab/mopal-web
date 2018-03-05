9a1e0b35544e02e9a3a132543cd1ee7f9a222d2d
Gabriel Desimone <gabriel.desimone@xcaleconsulting.com>
2018-03-05 19:43:09 -0300

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+const request = obj => {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open(obj.method || "GET", obj.url);
+        if (obj.headers) {
+            Object.keys(obj.headers).forEach(key => {
+                xhr.setRequestHeader(key, obj.headers[key]);
+            });
+        }
+        xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+        xhr.onload = () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject(xhr.response);
+            }
+        };
+        xhr.onerror = () => reject(xhr.statusText);
+        xhr.send(obj.body);
+    });
+};
+
+
+const saySomething = () => alert("Funca");
+
+const getUser = () => document.getElementById("user").value;
+const getPass = () => document.getElementById("pass").value;
+
+const api = 'http://localhost:3000/'
+
+
+const doLogin = () => {
+  let user = getUser();
+  let pass = getPass();
+  //alert(`Intentando login con usuario: ${user} y password: ${pass}`);
+
+  const body = {user, pass};
+
+  request({method: "POST", body: JSON.stringify(body), url: api + "user/login"})
+    .then(console.log)
+    .catch(err => {
+      let errorPlace = document.getElementById("errorPlace");
+      let obj = JSON.parse(err);
+      errorPlace.style.visibility = "visible";
+      errorPlace.innerHTML = "<h3>Error:</h3><p>" + obj.msg + "</p>";
+    });
+
+};
